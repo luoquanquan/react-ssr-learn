2eb112d9f4838f0fc75e8ae8c73a7f1b2d78c3ce
luoquanquan <luoquanquan@baidu.com>
2019-06-21 21:11:16 +0800

    feat: 客户端渲染配置 react-router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,41 @@
 import React from 'react'
+import {
+  Link, Switch, Route, HashRouter,
+} from 'react-router-dom'
+
+import Home from './Home.jsx'
+import About from './About.jsx'
+import Contact from './Contact.jsx'
+
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      title: '欢迎来到澳门皇冠赌场~',
+    }
   }
 
 
   render() {
+    const { title } = this.state
     return (
-      <div className="box">
-        这是我学习 SSR 的第一个 Demo
-      </div>
+      <HashRouter>
+        <div>
+          <h1>{ title }</h1>
+          <div>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
+
+          <main>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" exact component={About} />
+            <Route path="/contact" exact component={Contact} />
+          </main>
+        </div>
+      </HashRouter>
     )
   }
 }
